47aa1b5124f89fac9cd28f6a98c8fe3b03524a82
dpetrov <daniel.petrov@tick42.com>
2018-07-30 18:30:18 +0300

    draw phase

--- solution.js
+++ solution.js
@@ -1,39 +1,44 @@
-// import { DECK_SIZE, HEALTH, CARDS_IN_HAND, MANA } from 'constants'
-
 import { createNewPlayer } from "./createPlayer";
 import { HEALTH, MANA, DECK_SIZE, CARDS_IN_HAND } from './constants'
-printErr(createNewPlayer, 'create new player', HEALTH, MANA, DECK_SIZE)
+
 const player = createNewPlayer()
 const opponent = createNewPlayer()
 
 // game loop
 while (true) {
-  const playerInputs = readline().split(' ');
+  const playerInputs = readline().split(' ')
   player.set(HEALTH, parseInt(playerInputs[0]))
   player.set(MANA, parseInt(playerInputs[1]))
   player.set(DECK_SIZE, parseInt(playerInputs[2]))
 
-  const opponentInputs = readline().split(' ');
+  const opponentInputs = readline().split(' ')
   opponent.set(HEALTH, parseInt(opponentInputs[0]))
   opponent.set(MANA, parseInt(opponentInputs[1]))
   opponent.set(DECK_SIZE, parseInt(opponentInputs[2]))
 
-  let opponentHand = parseInt(readline());
-  let cardCount = parseInt(readline());
+  const opponentCardsCount = parseInt(readline())
+  opponent.set(CARDS_IN_HAND, opponentCardsCount)
+  const boardAndPlayersCardCount = parseInt(readline())
+  player.set(CARDS_IN_HAND, boardAndPlayersCardCount)
+
   for (let i = 0; i < cardCount; i++) {
-    let inputs = readline().split(' ');
-    let cardNumber = parseInt(inputs[0]);
-    let instanceId = parseInt(inputs[1]);
-    let location = parseInt(inputs[2]);
-    let cardType = parseInt(inputs[3]);
-    let cost = parseInt(inputs[4]);
-    let attack = parseInt(inputs[5]);
-    let defense = parseInt(inputs[6]);
-    let abilities = inputs[7];
-    let myHealthChange = parseInt(inputs[8]);
-    let opponentHealthChange = parseInt(inputs[9]);
-    let cardDraw = parseInt(inputs[10]);
+    const inputs = readline().split(' ')
+    const cardNumber = parseInt(inputs[0])
+    const instanceId = parseInt(inputs[1])
+    const location = parseInt(inputs[2])
+    const cardType = parseInt(inputs[3])
+    const cost = parseInt(inputs[4])
+    const attack = parseInt(inputs[5])
+    const defense = parseInt(inputs[6])
+    const abilities = inputs[7]
+    const myHealthChange = parseInt(inputs[8])
+    const opponentHealthChange = parseInt(inputs[9])
+    const cardDraw = parseInt(inputs[10])
+
+    if (player.get(MANA) === 0) { // DRAW PHASE
+
+    }
   }
 
-  print('PASS');
+  print('PASS')
 }
